6b8bfa9f93423885d643d7618df23e1a68d4b70d
Shumayl <83513359+mshumayl@users.noreply.github.com>
2022-12-03 00:26:57 +0800

    2022 Day Solution 2

--- solutions/2022/day_02/solution.py
+++ solutions/2022/day_02/solution.py
@@ -6,7 +6,6 @@ from ...base import StrSplitSolution, answer
 class Solution(StrSplitSolution):
     _year = 2022
     _day = 2
-
     
     @answer(13682)
     def part_1(self) -> int:
@@ -26,16 +25,19 @@ class Solution(StrSplitSolution):
             for key in dictionary.keys():
                 i = i.replace(key, dictionary[key])
 
-            i = i.split(' ')
+            i = i.split(" ")
 
-            res = int(i[0])-int(i[1])
+            for idx, j in enumerate(i):
+                i[idx] = int(j)
+
+            res = i[0] - i[1]
 
             if res==0:
-                point = int(i[1]) + 3
+                point = i[1] + 3
             elif res==1 or res==-2:
-                point = int(i[1])
+                point = i[1]
             elif res==2 or res==-1:
-                point = int(i[1]) + 6
+                point = i[1] + 6
             else:
                 raise ValueError
                         
@@ -44,10 +46,53 @@ class Solution(StrSplitSolution):
         return(sum(points))
     
     
-    # @answer(1234)
+    @answer(12881)
     def part_2(self) -> int:
         
-        pass
+        dictionary = {
+        "A": "1",
+        "B": "2",
+        "C": "3",
+        "X": "0",
+        "Y": "3",
+        "Z": "6"
+        }
+        
+        points = []
+
+        def get_losing_option(opp_choice):
+            if opp_choice==1:
+                return 3
+            else:
+                return opp_choice-1
+            
+        def get_winning_option(opp_choice):
+            if opp_choice==3:
+                return 1
+            else:
+                return opp_choice+1
+
+        for i in self.input:
+            for key in dictionary.keys():
+                i = i.replace(key, dictionary[key])
+
+            i = i.split(" ")
+            
+            for idx, j in enumerate(i):
+                i[idx] = int(j)
+
+            if i[1]==0: #if to lose
+                point = get_losing_option(i[0])
+            elif i[1]==3: #if to draw
+                point = i[0] + i[1]
+            elif i[1]==6: #if to win
+                point = get_winning_option(i[0]) + i[1]
+            else: 
+                raise ValueError
+
+            points.append(point)
+    
+        return sum(points)
 
     # @answer((1234, 4567))
     # def solve(self) -> Tuple[int, int]:
