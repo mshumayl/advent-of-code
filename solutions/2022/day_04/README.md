c97cda43107483aa8eb237ce59fcdbbe9641d5c7
Shumayl <83513359+mshumayl@users.noreply.github.com>
2022-12-04 15:49:08 +0800

    2022 Day 4 Solution 1

---
+++ solutions/2022/day_04/README.md
@@ -0,0 +1,13 @@
+# Day 4 (2022)
+
+`TITLE` ([prompt](https://adventofcode.com/2022/day/4))
+
+## Part 1
+In simple words, the requirements for this section is as such:
+1. For each line in the input, there are a pair of a set of numbers.
+2. The numbers are the range for each elf.
+3. Get lines where the range of either elf of each line is a subset of the other.
+   1. For e in range_a, if e is in range_b
+
+## Part 2
+
---
+++ solutions/2022/day_04/solution.py
@@ -0,0 +1,50 @@
+# prompt: https://adventofcode.com/2022/day/4
+
+from ...base import StrSplitSolution, answer
+# from typing import Tuple
+
+class Solution(StrSplitSolution):
+    _year = 2022
+    _day = 4
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        
+        reorg_ct = 0
+        
+        for i in self.input:
+            
+            elf_a_range = [int(i) for i in i.split(",")[0].split("-")]
+            elf_b_range = [int(i) for i in i.split(",")[1].split("-")]
+            
+            elf_a_sections = [i for i in range(elf_a_range[0], elf_a_range[1]+1)]
+            elf_b_sections = [i for i in range(elf_b_range[0], elf_b_range[1]+1)]
+
+            same = []
+            
+            #Check which range is longer in each pair
+            if len(elf_a_sections) < len(elf_b_sections):
+                for sect in elf_a_sections:
+                    if sect in elf_b_sections:
+                        same.append(sect)
+                    if same == elf_a_sections:
+                        reorg_ct+=1
+            elif len(elf_a_sections) > len(elf_b_sections):
+                for sect in elf_b_sections:
+                    if sect in elf_a_sections:
+                        same.append(sect)
+                    if same == elf_b_sections:
+                        reorg_ct+=1
+            else:
+                if elf_a_sections==elf_b_sections:
+                    reorg_ct+=1
+        
+        return reorg_ct        
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> Tuple[int, int]:
+    #     pass
