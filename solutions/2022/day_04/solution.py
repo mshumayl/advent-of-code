8e6b3b43fa6829ba710430f718606ec0d30e550d
Shumayl <83513359+mshumayl@users.noreply.github.com>
2022-12-04 21:14:51 +0800

    Code cleanup

--- solutions/2022/day_04/solution.py
+++ solutions/2022/day_04/solution.py
@@ -17,24 +17,30 @@ class Solution(StrSplitSolution):
             elf_a_range = [int(i) for i in i.split(",")[0].split("-")]
             elf_b_range = [int(i) for i in i.split(",")[1].split("-")]
             
-            elf_a_sections = [i for i in range(elf_a_range[0], elf_a_range[1]+1)]
-            elf_b_sections = [i for i in range(elf_b_range[0], elf_b_range[1]+1)]
+            elf_a_sections = range(elf_a_range[0], elf_a_range[1]+1)
+            elf_b_sections = range(elf_b_range[0], elf_b_range[1]+1)
 
             same = []
             
             #Check which range is longer in each pair
             if len(elf_a_sections) < len(elf_b_sections):
                 for sect in elf_a_sections:
-                    if sect in elf_b_sections:
+                    if sect not in elf_b_sections:
+                        break
+                    else:
                         same.append(sect)
-                    if same == elf_a_sections:
+                    if same == list(elf_a_sections):
                         reorg_ct+=1
+                        pass
             elif len(elf_a_sections) > len(elf_b_sections):
                 for sect in elf_b_sections:
-                    if sect in elf_a_sections:
+                    if sect not in elf_a_sections:
+                        break
+                    else:
                         same.append(sect)
-                    if same == elf_b_sections:
+                    if same == list(elf_b_sections):
                         reorg_ct+=1
+                        pass
             else:
                 if elf_a_sections==elf_b_sections:
                     reorg_ct+=1
