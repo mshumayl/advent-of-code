955742934a56218e162bbe018c04f1c708438a52
Shumayl <83513359+mshumayl@users.noreply.github.com>
2022-12-05 18:56:04 +0800

    Start Day 5

---
+++ solutions/2022/day_05/solution.py
@@ -0,0 +1,20 @@
+# prompt: https://adventofcode.com/2022/day/5
+
+from ...base import TextSolution, answer
+# from typing import Tuple
+
+class Solution(TextSolution):
+    _year = 2022
+    _day = 5
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> Tuple[int, int]:
+    #     pass
