96bebbdb4268fba7244c2d4085a0f8f76e561913
Shumayl <83513359+mshumayl@users.noreply.github.com>
2022-12-08 23:27:12 +0800

    2022 Day 8 Solution 1

--- solutions/2022/day_08/solution.py
+++ solutions/2022/day_08/solution.py
@@ -8,7 +8,7 @@ class Solution(StrSplitSolution):
     _year = 2022
     _day = 8
 
-    # @answer(1234)
+    @answer(1870)
     def part_1(self) -> int:
         matrix = []
         visible_trees = set()
@@ -26,55 +26,54 @@ class Solution(StrSplitSolution):
                 cols.append(matrix[rw][col])
             transposed_matrix.append(cols)
 
-
-        print(matrix)
-        print(transposed_matrix)
+        # print(matrix)
+        # print(transposed_matrix)
 
         def get_visible_trees(matrix, transposed: bool):
             for rix, row in enumerate(matrix):
                 for cix, column in enumerate(row):
                     # Current tree
-                    print(f"\nCurrent tree: {matrix[rix][cix]}")
+                    # print(f"\nCurrent tree: {matrix[rix][cix]}")
 
-                    if transposed:
-                        cix, rix = rix, cix
-
-                    right_diff = 0
-                    left_diff = 0
+                    right_diff = False
+                    left_diff = False
 
                     # From left
-                    print(f"Left: {matrix[rix][:cix]}")
+                    # print(f"Left: {matrix[rix][:cix]}")
                     try:
-                        left_diff = int(matrix[rix][cix]) - max(matrix[rix][:cix])
-                        print(f"Max: {max(matrix[rix][:cix])}")
+                        left_diff = int(matrix[rix][cix]) > max(matrix[rix][:cix])
+                        # print(f"Max: {max(matrix[rix][:cix])}")
+                        # print(left_diff)
                     except ValueError:
+                        left_diff=True
                         pass
                     # print(f"{left_diff=}")
                     
                     # From right
-                    print(f"Right: {matrix[rix][cix+1:]}")
+                    # print(f"Right: {matrix[rix][cix+1:]}")
                     try:
-                        right_diff = int(matrix[rix][cix]) - max(matrix[rix][cix+1:])
-                        print(f"Max: {max(matrix[rix][cix+1:])}")
+                        right_diff = int(matrix[rix][cix]) > max(matrix[rix][cix+1:])
+                        # print(f"Max: {max(matrix[rix][cix+1:])}")
+                        # print(right_diff)
                     except ValueError:
+                        right_diff=True
                         pass
                     # print(f"{right_diff=}")
                     
-                    if left_diff>=0 or right_diff>=0:
-                        visible_trees.add(f"{rix},{cix}")
+                    if left_diff or right_diff:
+                        # print("Visible!")
+                        if transposed:
+                            visible_trees.add(f"{rix},{cix}")
+                        else:
+                            visible_trees.add(f"{cix},{rix}")
 
-        
         get_visible_trees(matrix, False)
         get_visible_trees(transposed_matrix, True)
         
         ans = len(visible_trees)
-        tried_ans = [3234, 3831, 1727, 5127]
-        # print(f"\n{matrix=}")
-        if ans not in tried_ans:
-            print(f"\n{ans=}")
-        else:
-            print(f"\n{ans} has already been tried. Tried values -> {tried_ans}")
         # print(f"{visible_trees}")
+
+        return ans
         
     # @answer(1234)
     def part_2(self) -> int:
