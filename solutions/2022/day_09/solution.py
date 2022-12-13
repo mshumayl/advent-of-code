89875fc43d27c3e317069efc1a6c73fdefc93479
Shumayl <83513359+mshumayl@users.noreply.github.com>
2022-12-13 20:27:33 +0800

    Day 9 start

---
+++ solutions/2022/day_09/solution.py
@@ -0,0 +1,31 @@
+# prompt: https://adventofcode.com/2022/day/9
+
+from ...base import TextSolution, answer
+# from typing import Tuple
+
+class Solution(TextSolution):
+    _year = 2022
+    _day = 9
+
+    # @answer(1234)
+    def part_1(self) -> int:
+
+        # function to update location of tail based on head
+        def move(xt, yt, xh, yh):
+            pass
+
+        def check_if_touching():
+            pass
+        
+        def get_horizontal_direction():
+            pass
+            
+        pass
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        pass
+
+    # @answer((1234, 4567))
+    # def solve(self) -> Tuple[int, int]:
+    #     pass
